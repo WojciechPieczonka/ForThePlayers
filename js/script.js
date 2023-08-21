e7944711a186ea0eed782ee9c45f54b38d5ecf49
Wojciech Pieczonka <wojciechpieczonka@gmail.com>
2023-08-21 16:30:20 +0200

    animation added

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	const nav = document.querySelector('.navbar');
 	const allNavItems = document.querySelectorAll('.nav-link');
 	const navList = document.querySelector('.navbar-collapse');
+	const menuBtn = document.querySelector('.navbar-toggler');
 
 	function addShadow() {
 		if (window.scrollY >= 100) {
@@ -17,5 +18,10 @@ document.addEventListener('DOMContentLoaded', function () {
 		})
 	);
 
+	function addShadowOnClick() {
+		nav.classList.add('shadow-bg');
+	}
+
+	menuBtn.addEventListener('click', addShadowOnClick);
 	window.addEventListener('scroll', addShadow);
 });
